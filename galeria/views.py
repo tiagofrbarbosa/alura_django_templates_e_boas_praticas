15db8ad9037348ebdac46d3e56bc45f33c87bb21
Tiago Freire Barbosa <tfreirebar@gmail.com>
2024-10-20 23:37:22 -0300

    URL name

--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
